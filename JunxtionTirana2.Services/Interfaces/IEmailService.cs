a2e2bd80bed8101f31c440c63d2aa3c04d40d89c
Kedi Kalaj <93157449+kedikalaj@users.noreply.github.com>
2024-06-01 15:15:34 +0200

    Added application db context, migrations and project relations

---
+++ JunxtionTirana2.Services/Interfaces/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿
+using JunxtionTirana2.JunxtionTirana2.Models;
+
+namespace JunxtionTirana2.Services.Interfaces
+{
+    public interface IEmailService
+    {
+        void SendEmail(Message msg);
+    }
+}
--- JunxtionTirana2/Controllers/AuthenticationController.cs
+++ JunxtionTirana2/Controllers/AuthenticationController.cs
@@ -1,70 +1,73 @@
-﻿//using Microsoft.AspNetCore.Identity;
-//using Microsoft.AspNetCore.Mvc;
-//using JunxtionTirana2.Application.Interfaces;
+﻿using JunxtionTirana2.JunxtionTirana2.Models;
+using JunxtionTirana2.Model.Authentication;
+using JunxtionTirana2.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
-//namespace JunxtionTirana2.Controllers
-//{
-//    [Route("api/[controller]/[action]")]
-//    [ApiController]
-//    public class AuthenticationController : ControllerBase
-//    {
-//        private readonly UserManager<IdentityUser> _userManager;
-//        private readonly RoleManager<IdentityRole> _roleManager;
-//        private readonly IConfiguration _configuration;
-//        private readonly IEmailService _emailService;
 
-//        public AuthenticationController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration, IEmailService emailService)
-//        {
-//            _userManager = userManager;
-//            _roleManager = roleManager;
-//            _configuration = configuration;
-//            _emailService = emailService;
-//        }
+namespace JunxtionTirana2.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+        private readonly IEmailService _emailService;
 
-//        [HttpPost]
-//        public async Task<IActionResult> Register([FromBody] RegisterUserViewModel model, string role)
-//        {
-//            var userExists = await _userManager.FindByEmailAsync(model.Email);
-//            if (userExists != null)
-//            {
-//                return Unauthorized("The user already exists!");
-//            }
-//            IdentityUser user = new IdentityUser()
-//            {
-//                Email = model.Email,
-//                SecurityStamp = Guid.NewGuid().ToString(),
-//                UserName = model.Username
-//            };
-//            var result = await _userManager.CreateAsync(user, model.Password);
-//            if (!result.Succeeded)
-//            {
-//                return BadRequest("Could not create user.");
-//            }
+        public AuthenticationController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration, IEmailService emailService)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+            _emailService = emailService;
+        }
 
-//            if (await _roleManager.RoleExistsAsync(role))
-//            {
-//                var addroles = await _userManager.AddToRoleAsync(user, role);
-//                return Ok();
-//            }
-//            else
-//            {
-//                return BadRequest("The role doesnt exists.");
-//            }
-//        }
-//        [HttpPost]
-//        public IActionResult TestEmail()
-//        {
-//            try
-//            {
-//                string content = "<p>The content</p>";
-//                var msg = new Message(new string[] { "testemai@email.test" }, "Email test", content);
-//                _emailService.SendEmail(msg);
-//                return Ok("Message sent successfully");
-//            }
-//            catch
-//            {
-//                return BadRequest("Something went wrong!");
-//            }
-//        }
-//    }
-//}
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] RegisterUserViewModel model, string role)
+        {
+            var userExists = await _userManager.FindByEmailAsync(model.Email);
+            if (userExists != null)
+            {
+                return Unauthorized("The user already exists!");
+            }
+            IdentityUser user = new IdentityUser()
+            {
+                Email = model.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = model.Username
+            };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (!result.Succeeded)
+            {
+                return BadRequest("Could not create user.");
+            }
+
+            if (await _roleManager.RoleExistsAsync(role))
+            {
+                var addroles = await _userManager.AddToRoleAsync(user, role);
+                return Ok();
+            }
+            else
+            {
+                return BadRequest("The role doesnt exists.");
+            }
+        }
+        [HttpPost]
+        public IActionResult TestEmail()
+        {
+            try
+            {
+                string content = "<p>The content</p>";
+                var msg = new Message(new string[] { "testemai@email.test" }, "Email test", content);
+                _emailService.SendEmail(msg);
+                return Ok("Message sent successfully");
+            }
+            catch
+            {
+                return BadRequest("Something went wrong!");
+            }
+        }
+    }
+}
