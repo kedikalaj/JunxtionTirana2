12d859561e92239483ea28002648d41589d9ea65
Kedi Kalaj <93157449+kedikalaj@users.noreply.github.com>
2024-06-01 09:21:00 +0200

    Add project files.

---
+++ JunxtionTirana2.Application/Interfaces/IEmailService.cs
@@ -0,0 +1,15 @@
+﻿
+using JunxtionTirana2.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JunxtionTirana2.Application.Interfaces
+{
+    public interface IEmailService
+    {
+        void SendEmail(Message msg);
+    }
+}
---
+++ JunxtionTirana2/Model/Authentication/Response.cs
@@ -0,0 +1,14 @@
+﻿namespace JunxtionTirana2.Model.Authentication
+{
+    public class Response
+    {
+        /// <summary>
+        /// Gets or sets the status of the response
+        /// </summary>
+        public string? Status { get; set; }
+        /// <summary>
+        /// Gets or sets the message of the response
+        /// </summary>
+        public string? Message { get; set; }
+    }
+}
